3eea325556b77bb7b3d05d1cd4001d7ea21c2698
Under Ander <andryushin.alexander@gmail.com>
2021-05-01 18:40:25 +0700

    db relationships

--- db.py
+++ db.py
@@ -1,17 +1,23 @@
 from sqlalchemy import Integer, Column, String, Boolean, create_engine, ForeignKey
-from sqlalchemy.orm import relationship, joinedload, subqueryload, Session
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, Session
 
 Base = declarative_base()
-engine = create_engine('sqlite:///app.db?check_same_thread=False')
-session = sessionmaker(bind=engine)()
+engine = create_engine('sqlite+pysqlite:///app.db', echo=True, future=True)
+session = Session(engine)
 
 
 class Game(Base):
     __tablename__ = 'game'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String(50))
+    url = Column(String(50))
+
+    servers = relationship("Server", back_populates="game")
+    ads = relationship("Ad", back_populates="game")
+
+    def __repr__(self):
+        return f"Game(id={self.id!r}, name={self.name!r})"
 
 
 class Server(Base):
@@ -19,7 +25,13 @@ class Server(Base):
     id = Column(Integer, primary_key=True)
     server_id = Column(Integer)
     game_id = Column(Integer, ForeignKey('game.id'))
-    name = Column(String)
+    name = Column(String(50))
+
+    game = relationship("Game", back_populates="servers")
+    ads = relationship("Ad", back_populates="server")
+
+    def __repr__(self):
+        return f"Server(game_id={self.game_id!r}, name={self.name!r})"
 
 
 class Ad(Base):
@@ -27,8 +39,18 @@ class Ad(Base):
     id = Column(Integer, primary_key=True)
     game_id = Column(Integer, ForeignKey('game.id'))
     server_id = Column(Integer, ForeignKey('server.id'))
-    seller = Column(String)
+    seller = Column(String(50))
     side = Column(Integer)
     price = Column(Integer)
     amount = Column(Integer)
     online = Column(Boolean)
+
+    game = relationship("Game", back_populates="ads")
+    server = relationship("Server", back_populates="ads")
+
+    def __repr__(self):
+        return f"Ad(id={self.id!r}, seller={self.seller!r}, side={self.side!r}, price={self.price!r}," \
+               f" amount={self.amount!r}), online={self.online!r}"
+
+
+Base.metadata.create_all(engine)
