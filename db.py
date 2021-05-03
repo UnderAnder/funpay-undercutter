2cd565036c394ac8bf293a0a723460617403aae4
Under Ander <andryushin.alexander@gmail.com>
2021-05-03 22:39:01 +0700

    take the session as a param for testing purposes

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 
 from sqlalchemy import Integer, Column, String, Boolean, create_engine, ForeignKey, select, delete
 from sqlalchemy.ext.declarative import declarative_base
@@ -84,36 +84,36 @@ class Ad(Base):
 Base.metadata.create_all(engine)
 
 
-def check_table_fill(table: str, related: tuple = None) -> bool:
+def check_table_fill(table: str, related: tuple = None, session: Session = Session) -> bool:
     stmt = f'select * from {table} where {related[0]}={related[1]}' if related else f'select * from {table}'
-    with Session() as s:
+    with session() as s:
         if not s.execute(stmt).first():
             return False
         return True
 
 
-def get_game_by_name(name: str) -> Union[bool, Game]:
+def get_game_by_name(name: str, session: Session = Session) -> Union[None, Game]:
     stmt = select(Game).where(Game.name == name)
-    with Session() as s:
+    with session() as s:
         game = s.execute(stmt).first()
         if not game:
-            return False
+            return None
         return game[0]
 
 
-def get_server_by_name(name: str, game_id: int) -> Union[bool, Server]:
+def get_server_by_name(name: str, game_id: int, session: Session = Session) -> Union[None, Server]:
     stmt = select(Server).filter_by(name=name, game_id=game_id)
-    with Session() as s:
+    with session() as s:
         server = s.execute(stmt).first()
         if not server:
-            return False
+            return None
         return server[0]
 
 
-def get_ads_by_server(server_id: int) -> Union[bool, list]:
+def get_ads_by_server(server_id: int, session: Session = Session) -> Union[None, List[Ad]]:
     stmt = select(Ad).filter_by(server_id=server_id)
-    with Session() as s:
+    with session() as s:
         ads = s.execute(stmt).all()
         if not ads:
-            return False
+            return None
         return ads
