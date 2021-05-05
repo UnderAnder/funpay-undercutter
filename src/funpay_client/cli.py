0ce0c1b90a316532955152778ecd33added39666
Under Ander <andryushin.alexander@gmail.com>
2021-05-05 16:21:39 +0700

    get cookie from env

--- src/funpay_client/cli.py
+++ src/funpay_client/cli.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+from typing import Union
 
 
 def parse_args():
@@ -10,3 +12,22 @@ def parse_args():
     return parser.parse_args()
 
 
+def setup_cookie():
+    phpsessid_key = input('PHPSESSID key: ')
+    golden_key = input('GOLDEN key: ')
+    return phpsessid_key, golden_key
+
+
+def parse_cookie() -> Union[None, dict]:
+    # Get environment variables
+    phpsessid_key = os.getenv('FUNPAY_PHPSESSID')
+    golden_key = os.getenv('FUNPAY_GOLDEN')
+    if not any((phpsessid_key, golden_key)):
+        print('Cookie not setup, price change is impossible')
+        print('You can setup persistent cookie with FUNPAY_PHPSESSID and FUNPAY_GOLDEN system variables')
+        raw_input = input('Setup cookie for this session? y/n ')
+        if raw_input == 'y':
+            phpsessid_key, golden_key = setup_cookie()
+        else:
+            return None
+    return {'phpsessid': phpsessid_key, 'golden': golden_key}
