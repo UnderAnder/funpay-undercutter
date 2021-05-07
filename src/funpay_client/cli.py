106bfa7c52babed6f9bb75d23682419d97102bdf
Under Ander <andryushin.alexander@gmail.com>
2021-05-08 00:29:25 +0700

    edit ad

--- src/funpay_client/cli.py
+++ src/funpay_client/cli.py
@@ -5,6 +5,15 @@ from typing import Optional
 from funpay_client import parser, db, models
 
 
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Funpay funpay_client')
+    parser.add_argument('game', type=str, default='World of Warcraft RU, EU', nargs='?',
+                        metavar='Game name', help='e.g "World of Warcraft RU, EU"')
+    parser.add_argument('server', type=str, default='Азурегос', nargs='?', metavar='Server name',
+                        help='e.g "Silvermoon"')
+    return parser.parse_args()
+
+
 class Menu:
     def __init__(self, name: str, options: list = None, master=None, callback=None, back: bool = False):
         self.options = options if options is not None else []
@@ -36,7 +45,9 @@ class Menu:
         self.options += options
 
 
-def main_menu(game):
+def main_menu():
+    args = get_args()
+    game = db.get_game_by_name(args.game)
     menu = Menu(game.name, back=True)
     main_update_ads = Menu(f'Update data', callback=(parser.update_ads_for, game))
     main_set_lowest = Menu('Set all my lots at the lowest price', master=menu)
@@ -48,37 +59,44 @@ def main_menu(game):
     return menu
 
 
-def change_menu(game) -> Optional[Menu]:
+def change_menu(game) -> None:
     ad = select_ad(game)
     if not ad:
         return None
     menu = Menu('Change ad', master=main_menu, back=True)
-    change_set_lowest = Menu(f'Set ad to the lowest price', master=menu)
-    change_edit = Menu(f'Edit ad', master=menu)
+    change_set_lowest = Menu('Set ad to the lowest price', master=menu)
+    change_edit = Menu('Edit ad', callback=(edit_ad, ad))
     menu_back = Menu('Back', callback=main_menu)
 
     menu.add_options(change_set_lowest, change_edit, menu_back)
-    return menu
+    menu()
 
 
 def select_ad(game) -> Optional[models.Ad]:
     user_name = parser.get_user_name()
+    if not user_name:
+        return None
     user_ads = db.get_ads_for(user_name, game.id)
     if not user_ads:
         return None
     print('\n'.join(f'{i}. {ad[0]}' for i, ad in enumerate(user_ads, start=1)))
     allowed = tuple(str(i) for i in range(1, len(user_ads) + 1))
     user_choice = check_input('Select one: ', proper_values=allowed)
-    return user_ads[int(user_choice)][0]
+    return user_ads[int(user_choice) - 1][0]
 
 
-def args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Funpay funpay_client')
-    parser.add_argument('game', type=str, default='World of Warcraft RU, EU', nargs='?',
-                        metavar='Game name', help='e.g "World of Warcraft RU, EU"')
-    parser.add_argument('server', type=str, default='Азурегос', nargs='?', metavar='Server name',
-                        help='e.g "Silvermoon"')
-    return parser.parse_args()
+def edit_ad(ad: models.Ad):
+    def price_without_commission(price) -> float:
+        return round((price - price * 0.19), 3)
+    price = float(input('Price with commission (leave blank to save old price): '))
+    if price:
+        without_commission = price_without_commission(price)
+        ad.price = without_commission
+        print('Price without commission:', without_commission)
+    amount = int(input('Amount (leave blank to save old price): '))
+    if amount:
+        ad.amount = amount
+    return parser.set_values_for(ad)
 
 
 def setup_cookie():
@@ -111,4 +129,5 @@ def check_input(*args, proper_values: tuple, back: bool = False, **kwargs) -> Op
             print(raw, 'is not an option')
             if back:
                 return
+            continue
         return raw
